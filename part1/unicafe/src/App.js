198f409849ff9a04fefe92840e8f04ef3c992be1
ed-w-ds <e.lisovskis@se22.qmul.ac.uk>
2023-06-23 16:40:37 +0100

    unicafe step 2 complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,6 +8,21 @@ const Button = ( {text, handleClick} ) => {
   )
 }
 
+const Statistics = ( {good, neutral, bad} ) => {
+  const all = good + bad + neutral;
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good: { good }</p>
+      <p>Neutral: { neutral }</p>
+      <p>Bad: { bad }</p>
+      <p>all { all }</p>
+      <p>average { ((good - bad) / (all)).toFixed(2) }</p>
+      <p>positive { (good / all * 100).toFixed(2) }%</p>
+    </div>
+  )
+}
+
 
 const App = () => {
   // save clicks of each button to its own state
@@ -21,10 +36,13 @@ const App = () => {
       <Button handleClick={ () => setGood(good + 1) } text='Good' />
       <Button handleClick={ () => setNeutral(neutral + 1) } text='Neutral' />
       <Button handleClick={ () => setBad(bad + 1) } text='Bad' />
-      <h2>Statistics</h2>
+      {/* <h2>Statistics</h2>
       <p>Good: { good }</p>
       <p>Neutral: { neutral }</p>
       <p>Bad: { bad }</p>
+      <p>all { good + bad + neutral }</p>
+      <p> average { ((good - bad) / (good + bad + neutral).toFixed(2)) }</p> */}
+      <Statistics good={ good } neutral={ neutral } bad={ bad } />
     </div>
   )
 }

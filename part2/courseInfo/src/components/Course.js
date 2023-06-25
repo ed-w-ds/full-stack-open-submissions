1f2d5d62a1b5093f46f0234191a9bb4a6b4a86f4
ed-w-ds <e.lisovskis@se22.qmul.ac.uk>
2023-06-25 18:50:25 +0100

    style changes

--- part2/courseInfo/src/components/Course.js
+++ part2/courseInfo/src/components/Course.js
@@ -1,40 +1,40 @@
 const Course = ({ course }) => {
-    return (
-      <div>
-        <Header course={course} />
-        <Content course={course} />
-        <Total course={course} />
-      </div>
-    )
-  }
+return (
+    <div>
+    <Header course={course} />
+    <Content course={course} />
+    <Total course={course} />
+    </div>
+)
+}
 
-  const Header = ({ course }) => {
-    return (
-      <h1>{course.name}</h1>
-    )
-  }
-  
-  const Content = ({ course }) => {
-    return (
-      <div>
-        <Part course={course} />
-      </div>
-    )
-  }
+const Header = ({ course }) => {
+return (
+    <h1>{course.name}</h1>
+)
+}
 
-  const Part  = ({ course }) => {
-    return (
-      <div>
-        {course.parts.map(part => <p key={ part.id }>{ part.name } { part.exercises }</p>)}
-      </div>
-    )
-  }
-  
-  const Total = ({ course }) => {
-    const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-    return (
-      <p><b>total of { total } exercises</b></p>
-    )
-  }
-  
+const Content = ({ course }) => {
+return (
+    <div>
+    <Part course={course} />
+    </div>
+)
+}
+
+const Part  = ({ course }) => {
+return (
+    <div>
+    {course.parts.map(part => <p key={ part.id }>{ part.name } { part.exercises }</p>)}
+    </div>
+)
+}
+
+const Total = ({ course }) => {
+const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+return (
+    <p><b>total of { total } exercises</b></p>
+)
+}
+
 export default Course

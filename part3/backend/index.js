4695138e11862f1fa2a3d846729bf4e0260e83b3
ed-w-ds <e.lisovskis@se22.qmul.ac.uk>
2023-07-17 13:55:18 +0100

    ex3.16 complete

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -35,9 +35,20 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Phonebook.findById(request.params.id).then(phonebook => {
     response.json(phonebook)
+  })
+  .then (phonebook => {
+    if (phonebook) {
+      response.json(phonebook)
+    }
+    else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => {
+    next(error)
   })
 })
 
@@ -75,9 +86,25 @@ app.post('/api/persons', (request, response) => {
     phonebook.save().then(savedPerson => {
       response.json(savedPerson)
     })
+    
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const phonebook = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Phonebook.findByIdAndUpdate(request.params.id, phonebook, { new: true })
+    .then(updatedPhonebook => {
+      response.json(updatedPhonebook)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   persons = persons.filter(person => person.id !== id)
 
@@ -85,9 +112,23 @@ app.delete('/api/persons/:id', (request, response) => {
     .then(result => {
       response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  // if the error is cast error, then it is a bad request
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {

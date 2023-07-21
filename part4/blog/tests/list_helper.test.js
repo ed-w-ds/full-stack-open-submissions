f83d0a49e2bc617a9f2919e43b024067c0efa27c
ed-w-ds <e.lisovskis@se22.qmul.ac.uk>
2023-07-21 20:46:27 +0100

    ex4.5 complete

--- part4/blog/tests/list_helper.test.js
+++ part4/blog/tests/list_helper.test.js
@@ -10,6 +10,7 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
+  
   const blogs = [
     {
       _id: "5a422a851b54a676234d17f7",
@@ -61,9 +62,36 @@ describe('total likes', () => {
     }  
   ]
 
-  test('when list has only one blog, equals the likes of that', () => {
+  test('total likes of all posts', () => {
     const result = listHelper.totalLikes(blogs)
-    console.log(result)
     expect(result).toBe(36)
+  })
+})
+
+describe('return blog with most likes', () => {
+  const blogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('the blog with most likes', () => {
+    const result = listHelper.favPost(blogs)
+    
+    expect(result).toEqual(
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+      }
+    )
   })
 })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -3,14 +3,23 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-    console.log(blogs)
     return blogs.reduce(
         (total, blogs) => {
             return total + blogs.likes
         }, 0)
 }
 
+const favPost = (blogs) => {
+    return blogs.reduce(
+        (fav, blogs) => {
+            return fav.likes > blogs.likes 
+            ? fav 
+            : blogs
+        }, {})
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    favPost,
 }

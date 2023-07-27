9436946fc1192544e9b168e065899d5bf3a8ac41
ed-w-ds <e.lisovskis@se22.qmul.ac.uk>
2023-07-27 16:00:56 +0100

    ex5.1 complete

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -60,19 +60,22 @@ const App = () => {
     </form>
   )
 
+  const showBlogs = () => (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+
+
   return (
     <>
       {user === null 
         ? loginForm() 
-        : <p>{user.name} logged-in</p>
+        : showBlogs()
       }
-
-      <div>
-        <h2>blogs</h2>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
-        )}
-      </div>
     </>
   )
 }

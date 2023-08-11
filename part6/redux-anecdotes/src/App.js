70c5aceac778750937a7104445aa799caf6c0cb8
ed-w-ds <e.lisovskis@se22.qmul.ac.uk>
2023-08-11 20:41:56 +0100

    ex6.9 complete

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,33 +1,8 @@
-import { useSelector } from 'react-redux'
-import { useDispatch } from 'react-redux'
-import { filterChange } from './reducers/filterReducer'
-
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import Filter from './components/Filter'
 
 const App = () => {
-
-  const Filter = () => {
-    const filter = useSelector(state => state.filter)
-    const dispatch = useDispatch()
-
-    const handleChange = (event) => {
-      // input-field value is in variable event.target.value
-      dispatch(filterChange(event.target.value))
-    }
-    const style = {
-      marginBottom: 10
-    }
-
-    return (
-      <div style={style}>
-        filter <input
-          value={filter}
-          onChange={handleChange}
-        />
-      </div>
-    )
-  }
 
   return (
     <div>
---
+++ part6/redux-anecdotes/src/components/Filter.js
@@ -0,0 +1,25 @@
+import { useSelector, useDispatch } from "react-redux"
+import { filterChange } from "../reducers/filterReducer"
+
+const Filter = () => {
+    const filter = useSelector(state => state.filter)
+    const dispatch = useDispatch()
+
+    const handleChange = (event) => {
+      // input-field value is in variable event.target.value
+      dispatch(filterChange(event.target.value))
+    }
+    const style = {
+      marginBottom: 10
+    }
+    return (
+        <div style={style}>
+          filter <input
+            value={filter}
+            onChange={handleChange}
+          />
+        </div>
+      )
+    }
+
+export default Filter

1342cc97347c8d0466f484af2bd387c1490d3947
ed-w-ds <e.lisovskis@se22.qmul.ac.uk>
2023-08-12 14:36:22 +0100

    ex6.10 complete

--- part6/redux-anecdotes/src/components/Filter.js
+++ part6/redux-anecdotes/src/components/Filter.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux"
-import { filterChange } from "../reducers/filterReducer"
+import { setFilter } from "../reducers/filterReducer"
 
 const Filter = () => {
 
@@ -9,7 +9,7 @@ const Filter = () => {
 
     const handleChange = (event) => {
       // input-field value is in variable event.target.value
-      dispatch(filterChange(event.target.value))
+      dispatch(setFilter(event.target.value))
     }
     const style = {
       marginBottom: 10

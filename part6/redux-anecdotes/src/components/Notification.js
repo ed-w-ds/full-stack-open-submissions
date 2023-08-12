cfb660adc6c375a574ae37eb243197b2ec6e81ac
ed-w-ds <e.lisovskis@se22.qmul.ac.uk>
2023-08-12 15:51:44 +0100

    ex6.13 complete

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,5 +1,4 @@
 import { useSelector } from 'react-redux'
-// import { setNotification } from '../reducers/notificationReducer'
 
 const Notification = () => {
   const notification = useSelector(state => state.notification)

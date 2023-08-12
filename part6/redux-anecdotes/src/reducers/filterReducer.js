2eb8e35c13358d6a432ae4ce78cb27d7136b3c5e
ed-w-ds <e.lisovskis@se22.qmul.ac.uk>
2023-08-12 14:36:22 +0100

    ex6.10&11 complete

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,19 +1,34 @@
-// filter anecdotes by text input
-const filterReducer = (state = '', action) => {
-    switch (action.type) {
-        case 'SET_FILTER':
-        return action.payload
-        default:
-            return state
-    }
-}
+import { createSlice } from '@reduxjs/toolkit'
 
-// action creator
-export const filterChange = filter => {
-    return {
-        type: 'SET_FILTER',
-        payload: filter,
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState: '',
+    reducers: {
+        setFilter(state, action) {
+            return action.payload
+        }
     }
-}
+})
 
-export default filterReducer
+export const { setFilter } = filterSlice.actions
+export default filterSlice.reducer
+
+// // filter anecdotes by text input
+// const filterReducer = (state = '', action) => {
+//     switch (action.type) {
+//         case 'SET_FILTER':
+//         return action.payload
+//         default:
+//             return state
+//     }
+// }
+
+// // action creator
+// export const filterChange = filter => {
+//     return {
+//         type: 'SET_FILTER',
+//         payload: filter,
+//     }
+// }
+
+// export default filterReducer

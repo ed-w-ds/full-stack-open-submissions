32586c9f927c85d676776ef30e595d35529acce1
ed-w-ds <e.lisovskis@se22.qmul.ac.uk>
2023-08-13 16:10:22 +0100

    ex6.20 intial solution

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,19 +1,40 @@
+// libraries and frameworks
+import { useQuery } from 'react-query'
+
+// components
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
+// requests
+import { getAnecdotes } from './requests'
+
 const App = () => {
+  const { data: anecdotes, status } = useQuery(
+    // queryKey
+    'anecdotes', 
+    // queryFn
+    getAnecdotes,
+    {
+      // options
+      retry: 1
+    })
+  // const anecdotes = data is the same as using data: anecdotes in the object
+
+  if (status === 'loading') {
+    return <div>Loading anecdotes...</div>
+  }
+
+  if (status === 'error') {
+    return <div>anecdote service not available due to problems in the server</div>
+  }
+
+  console.log('anecdotes', anecdotes)
+
+
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
-
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
 
   return (
     <div>

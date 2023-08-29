079920423da33158b4a5e120ae638b9690d26260
ed-w-ds <e.lisovskis@se22.qmul.ac.uk>
2023-08-29 15:35:29 +0100

    ex8.6 complete

--- part8/library/library-backend.js
+++ part8/library/library-backend.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -118,6 +119,15 @@ const typeDefs = `
         allBooks(author: String, genre: String ): [Book!]!
         allAuthors: [Author!]!
     }
+
+    type Mutation {
+        addBook(
+            title: String!
+            author: String!
+            published: Int!
+            genres: [String!]!
+        ): Book
+    }
 `
 
 const resolvers = {
@@ -151,6 +161,17 @@ const resolvers = {
     id: (root) => root.id,
     bookCount: (root) => books.filter(b => b.author === root.name).length
   },
+    Mutation: {
+        addBook: (root, args) => {
+            if (!authors.find(a => a.name === args.author)) {
+                const author = { name: args.author, id: uuid() }
+                authors = authors.concat(author)
+            }
+            const book = { ...args, id: uuid() }
+            books = books.concat(book)
+            return book
+        }
+    }
 }
 
 const server = new ApolloServer({
